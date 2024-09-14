f21c3a593aead41f7b39d8fe130bd1dfc8da2ca3
SShipon <nxshipon@gmail.com>
2024-09-14 07:00:21 -0700

    shose data updated

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+
 import React from 'react';
 
 const HomePage = async () => {
@@ -8,24 +8,21 @@ const HomePage = async () => {
     <div>
        <h1 className='text-2xl font-semibold text-center'>Hello home Page next js</h1>
      
-       <div className='flex justify-between p-8'>
+       <div className='flex justify-between p-5'>
         {
           shoes.slice(0,3).map(shoe => 
           <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
             <figure>
-              <Image
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes" width={500}  height={200}/>
+             <img src="" alt="" />
             </figure>
             <div className="card-body">
               <h2 className="card-title">
-                Shoes!
-                <div className="badge badge-secondary">NEW</div>
+                {shoe.title}
               </h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <p>{shoe.description}</p>
               <div className="card-actions justify-end">
-                <div className="badge badge-outline">Fashion</div>
-                <div className="badge badge-outline">Products</div>
+                <div className="btn bg-primary">Fashion</div>
+                <div className="btn bg-accent">Products</div>
               </div>
             </div>
           </div>)
